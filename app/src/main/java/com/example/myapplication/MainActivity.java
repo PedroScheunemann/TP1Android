dd6ede47b52f02a43b20aa7e698011e59050a7b6
Pedro Scheunemann <pedroscheunemann2001@gmail.com>
2023-06-08 23:53:40 -0300

    Parte 1 - Exercício 3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,32 +3,86 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    Button buttonEnter;
-    EditText inputText;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+
+    EditText editX, editY, editZ;
+
+    float xAccel, yAccel, zAccel, xPrevAccel, yPrevAccel, zPrevAccel, accelThreshold = 2.5f;
+    boolean firstMeasure = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        inputText = findViewById(R.id.inputText);
-        buttonEnter = findViewById(R.id.buttonEnter);
-        buttonEnter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        editX = findViewById(R.id.editX);
+        editY = findViewById(R.id.editY);
+        editZ = findViewById(R.id.editZ);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType()== Sensor.TYPE_ACCELEROMETER) {
+            editX.setText("X: "+ String.valueOf(event.values[0]));
+            editY.setText("Y: "+ String.valueOf(event.values[1]));
+            editZ.setText("Z: "+ String.valueOf(event.values[2]));
+            updateAccelMeasures(event.values[0], event.values[1], event.values[2]);
+            if(accelerationDetected()){
                 Intent intent = new Intent(getApplicationContext(), SndActivity.class);
-                intent.putExtra("data", inputText.getText().toString());
                 startActivity(intent);
             }
-        });
+        }
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy)
+    {
+    }
 
+    private void updateAccelMeasures(float x, float y, float z){
+        if (firstMeasure){
+            xPrevAccel = x;
+            yPrevAccel = y;
+            zPrevAccel = z;
+            firstMeasure = false;
+        }
+        else{
+            xPrevAccel = xAccel;
+            yPrevAccel = yAccel;
+            zPrevAccel = zAccel;
+        }
+        xAccel = x;
+        yAccel = y;
+        zAccel = z;
+    }
+
+    private boolean accelerationDetected(){
+        float deltaX = Math.abs(xAccel - xPrevAccel);
+        float deltaY = Math.abs(yAccel - yPrevAccel);
+        float deltaZ = Math.abs(zAccel - zPrevAccel);
+        return (deltaX > accelThreshold) || (deltaY > accelThreshold) || (deltaZ > accelThreshold);
     }
 }
--- app/src/main/java/com/example/myapplication/SndActivity.java
+++ app/src/main/java/com/example/myapplication/SndActivity.java
@@ -7,16 +7,9 @@ import android.widget.TextView;
 
 public class SndActivity extends AppCompatActivity {
 
-    TextView message;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snd);
-
-        message = findViewById(R.id.messageView);
-        String dato = getIntent().getStringExtra("data");
-        message.setText(dato);
-
     }
 }
