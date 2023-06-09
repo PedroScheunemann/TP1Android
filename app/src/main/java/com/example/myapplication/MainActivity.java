3c6b06715a16ba9662632f665375017ec6456e45
Pedro Scheunemann <pedroscheunemann2001@gmail.com>
2023-06-08 21:59:42 -0300

    Parte 1 - Exercício 2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,29 +11,24 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button buttonSum;
-    TextView textResult;
-    EditText firstNumber;
-    EditText secondNumber;
+    Button buttonEnter;
+    EditText inputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        firstNumber = findViewById(R.id.textFirstNumber);
-        secondNumber = findViewById(R.id.textSecondNumber);
-        textResult = findViewById(R.id.textViewResult);
-
-        buttonSum = findViewById(R.id.buttonSum);
-        buttonSum.setOnClickListener(new View.OnClickListener() {
+        inputText = findViewById(R.id.inputText);
+        buttonEnter = findViewById(R.id.buttonEnter);
+        buttonEnter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int a = Integer.parseInt(firstNumber.getText().toString());
-                int b = Integer.parseInt(secondNumber.getText().toString());
-                int sum = a + b;
-                textResult.setText("RESULT: " + sum);
+                Intent intent = new Intent(getApplicationContext(), SndActivity.class);
+                intent.putExtra("data", inputText.getText().toString());
+                startActivity(intent);
             }
         });
+
     }
 }
